a22a876b73cee3ffcf6a3cacfb8a67b7886bcecd
dinbruno <brunodinosantos@outlook.com>
2023-05-09 19:15:15 -0300

    feat: first login page using hookform + yup

---
+++ src/components/Admin/LoginForm/login.tsx
@@ -0,0 +1,67 @@
+'use client'
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { LoginSchema } from './login.schema';
+
+interface FormData {
+  email: string;
+  password: string;
+}
+
+const LoginForm: React.FC = () => {
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: yupResolver(LoginSchema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md mx-auto">
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">E-mail:</label>
+        <Controller
+          control={control}
+          name="email"
+          render={({ field }) => (
+            <input
+              {...field}
+              type="email"
+              id="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="E-mail"
+            />
+          )}
+        />
+        {errors.email && <p className="text-red-500 text-xs italic">{errors.email.message}</p>}
+      </div>
+      <div className="mb-6">
+        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Senha:</label>
+        <Controller
+          control={control}
+          name="password"
+          render={({ field }) => (
+            <input
+              {...field}
+              type="password"
+              id="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Senha"
+            />
+          )}
+        />
+        {errors.password && <p className="text-red-500 text-xs italic">{errors.password.message}</p>}
+      </div>
+      <div className="flex items-center justify-between">
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Entrar
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
