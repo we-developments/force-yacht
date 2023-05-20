c335c9ee0b4fe8ef2acd8b5097a730dd938ab913
dinbruno <brunodinosantos@outlook.com>
2023-05-19 21:47:41 -0300

    feat: all

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import mainBanner from "../src/images/pngs/banner3.jpg";
+import banner5 from "../src/images/pngs/banner5.jpg";
 import yt1 from "../src/images/pngs/y1.jpg";
 import yt2 from "../src/images/pngs/y2.jpg";
 import yt3 from "../src/images/pngs/y3.jpg";
@@ -8,11 +9,22 @@ import yt4 from "../src/images/pngs/y4.jpg";
 import yt5 from "../src/images/pngs/y5.jpg";
 import yt6 from "../src/images/pngs/y6.jpg";
 import Logo from "../src/images/pngs/force.png";
+import Barco from "../src/images/pngs/barco.png";
+import manBoat from "../src/images/pngs/manboat.png";
+import bgFooter from "../src/images/pngs/bg-footer.jpg";
+import Ocean from "../src/images/pngs/bg-6.webp";
+import jumpGirls from "../src/images/pngs/jump-girls.png";
+import Brinde from "../src/images/pngs/brinde.jpg";
+import ForceInstagram from "../src/images/pngs/force-instagram_cropped.png";
 import Image from "next/image";
 import { useSpring, animated } from "react-spring";
 import Link from "next/link";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { useIconGetter } from "../src/hooks/useIconGetter/useIconGetter";
+import Footer from "@/src/components/Footer/footer";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 
 export default function HomePage() {
   const fade = useSpring({
@@ -20,18 +32,6 @@ export default function HomePage() {
     to: { opacity: 1 },
     delay: 300,
   });
-
-  const [scrolled, setScrolled] = useState(false);
-
-  const handleScroll = () => {
-    const offset = window.scrollY;
-
-    if (offset > 100) {
-      setScrolled(true);
-    } else {
-      setScrolled(false);
-    }
-  };
 
   const images = [
     {
@@ -78,6 +78,22 @@ export default function HomePage() {
     },
   ];
 
+  const { Icon } = useIconGetter();
+
+  const [scrolled, setScrolled] = useState(false);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+
+    if (offset > 100) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -96,10 +112,6 @@ export default function HomePage() {
     },
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-  });
-
   let navbarClasses = [
     "fixed",
     "top-0",
@@ -116,10 +128,9 @@ export default function HomePage() {
 
   if (scrolled) {
     navbarClasses.push(
-      "bg-accent",
+      "bg-primary",
       "h-16",
       "w-full",
-      "bg-accent",
       "bg-clip-padding",
       "backdrop-filter",
       "backdrop-blur-md",
@@ -128,9 +139,31 @@ export default function HomePage() {
     logoHeight = 50;
   }
 
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  const openVideo = (videoUrl: any) => {
+    setSelectedVideo(videoUrl);
+  };
+
   return (
     <div className="z-10" style={{ height: "200vh" }}>
-      <header className={navbarClasses.join(" ")}>
+      <motion.header
+        className={navbarClasses.join(" ")}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
         <nav className="flex">
           <div className="px-4 py-2 flex justify-left round">
             <ul className="flex space-x-2 sm:space-x-4 gap-8">
@@ -148,17 +181,31 @@ export default function HomePage() {
               </li>
               <li className="text-sm sm:text-base text-center flex items-center">
                 <a href="#" className="text-white">
+                  Sobre nós
+                </a>
+              </li>
+              <li className="text-sm sm:text-base text-center flex items-center">
+                <a href="#" className="text-white">
+                  Nossas embarcações
+                </a>
+              </li>
+              <li className="text-sm sm:text-base text-center flex items-center">
+                <a href="#" className="text-white">
                   Clientes
                 </a>
               </li>
             </ul>
           </div>
         </nav>
-      </header>
-      <Image src={mainBanner} alt="banner" className="h-screen w-screen" />
+      </motion.header>
+      <Image
+        src={banner5}
+        alt="banner"
+        className="h-screen w-screen object-cover"
+      />
       <div
         className="absolute left-4 sm:left-8 text-left z-0"
-        style={{ top: "15%", left: "10%" }}
+        style={{ top: "25%", left: "10%" }}
       >
         <animated.h1
           style={fade}
@@ -168,9 +215,10 @@ export default function HomePage() {
         </animated.h1>
         <animated.h1
           style={fade}
-          className="text-secondary lg:text-8xl sm:text-5xl font-Marcellus flex gap-10"
+          className="text-primary lg:text-6xl sm:text-5xl font-Marcellus flex gap-2 "
         >
-          Navegue
+          <Icon icon="wheel" />
+          <span className="items-center flex">Navegue</span>
           <animated.p
             style={fade}
             className="text-lg sm:text-sm text-white font-Marcellus flex items-center"
@@ -188,47 +236,247 @@ export default function HomePage() {
         </animated.h1>
       </div>
 
-      <section>
-        <div className="flex justify-center items-center h-72">
-          <div className="flex flex-col justify-center items-center">
-            <h1 className="text-8xl sm:text-5xl lg:text-8xl text-center font-Marcellus">
-              Nossos Serviços
-            </h1>
-            <p className="text-lg sm:text-2xl text-center font-Marcellus">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptatum, quibusdam, quia, quod voluptates quos voluptate
-              voluptatibus quas doloribus quidem fugiat. Quisquam voluptatum,
-              quibusdam, quia, quod voluptates quos voluptate voluptatibus quas
-              doloribus quidem fugiat.
-            </p>
+      <section className="h-96">
+        <div className="flex justify-center items-center bg-white h-full">
+          <div className="flex flex-col justify-center items-center relative">
+            <motion.div
+              ref={ref}
+              initial={{ x: "-50vw" }}
+              animate={controls}
+              transition={{ type: "spring", stiffness: 60 }}
+              variants={{
+                visible: { x: 0 },
+                hidden: { x: "-50vw" },
+              }}
+              className="absolute z-0 left-0"
+            >
+              <Image src={Barco} alt="barco" />
+            </motion.div>
+            <div className="flex justify-center">
+              <div className="w-2/3 z-20">
+                <div className="flex justify-center">
+                  <Icon icon="anchor" />
+                </div>
+                <h1 className="sm:text-5xl lg:text-4xl text-center font-Marcellus font-bold text-primary">
+                  Sobre nós
+                </h1>
+                <div className="w-8 h-1 mt-2 ml-1 mx-auto bg-primary"></div>
+
+                <p className="lg:text-base sm:text-2xl text-left font-Marcellus pt-4">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam voluptatum, quibusdam, quia, quod voluptates quos
+                  voluptate voluptatibus quas doloribus quidem fugiat. Quisquam
+                  voluptatum, quibusdam, quia, quod voluptates quos voluptate
+                  voluptatibus quas doloribus quidem fugiat.
+                </p>
+              </div>
+            </div>
+            <div className="grid grid-cols-4">
+              {/* A grid of items and icons */}
+
+              <div className="col-span-1 flex justify-center items-center">
+                <p className="text-center">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <Icon icon="anchor" />
+              </div>
+              <div className="col-span-1 flex justify-center items-center">
+                <p className="text-center">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <Icon icon="anchor" />
+              </div>
+            </div>
           </div>
         </div>
       </section>
 
-      <section>
-          <Carousel
-            swipeable={false}
-            draggable={false}
-            showDots={true}
-            responsive={responsive}
-            ssr={true}
-            infinite={true}
-            autoPlay={false}
-            autoPlaySpeed={1000}
-            keyBoardControl={true}
-            customTransition="all .9s ease-in-out"
-            transitionDuration={900}
-            containerClass="carousel-container"
-            dotListClass="custom-dot-list-style bg-transparent"
-            itemClass="carousel-item-padding-40-px w-full"
-          >
-            {images.map((image, index) => (
-              <div key={index}>
-                <Image src={image.image} width={700} alt={image.title}/>
-              </div>
-             ))}
-          </Carousel>
+      <section className="w-full bg-off py-10">
+        <div className="grid grid-cols-2 w-4/5 mx-auto pt-20 min-">
+          <div className="col-span-1 flex justify-center relative z-10 bg-white">
+            <div className="p-8">
+              <h1 className="text-4xl font-bold text-primary">Serviços</h1>
+              <div className="w-8 h-1 mt-2 ml-1 flex justify-start bg-primary"></div>
+              <p className="text-lg pt-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam voluptatum, quibusdam, quia, quod voluptates quos
+                voluptate voluptatibus quas doloribus quidem fugiat. Quisquam
+                voluptatum, quibusdam, quia, quod voluptates quos voluptate
+                voluptatibus quas doloribus quidem fugiat.
+              </p>
+            </div>
+          </div>
+          <div className="col-span-1 z-20 relative bg-white">
+            <Image src={manBoat} alt="boat" className="w-full" />
+          </div>
+        </div>
       </section>
+
+      <section className="px-4 h-screen bg-off">
+        <div>
+          <div className="text-left mb-8 lg:p-4">
+            <h2 className="text-4xl font-bold text-primary">
+              Conheça nossas Embarcações
+            </h2>
+            <div className="w-8 h-1 mt-2 ml-1 flex justify-start bg-primary"></div>
+          </div>
+
+          <div>
+            <Carousel
+              swipeable={true}
+              draggable={true}
+              showDots={true}
+              responsive={responsive}
+              ssr={true}
+              infinite={true}
+              autoPlay={false}
+              autoPlaySpeed={1000}
+              keyBoardControl={true}
+              customTransition="all .9s ease-in-out"
+              transitionDuration={900}
+              containerClass="carousel-container"
+              dotListClass="custom-dot-list-style bg-transparent"
+              itemClass="carousel-item-padding-40-px w-full px-4"
+            >
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className="bg-transparent shadow my-10 h-full hover:bg-white transition-all ease-out delay-200"
+                >
+                  <Image
+                    src={image.image}
+                    alt={image.title}
+                    className="object-cover h-64 w-full "
+                  />
+                  <div className="mt-2 p-8 relative ">
+                    <span className="flex gap-2 text-sm text-gray-400">
+                      <Icon icon="location" svgProps={{ fill: "#DBDFE4" }} />
+                      PORTO BELO
+                    </span>
+                    <h2 className="text-xl font-bold py-4 text-primary">
+                      {image.title}
+                    </h2>
+                    <p>{image.description}</p>
+                  </div>
+                </div>
+              ))}
+            </Carousel>
+          </div>
+        </div>
+      </section>
+
+      <section className="h-[30rem] bg-off">
+        <div className="grid grid-cols-2 w-4/5 mx-auto">
+          <div
+            className="grid grid-rows-5 grid-columns-6 gap-2 h-full relative cursor-pointer"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            onClick={() => openVideo("url do seu vídeo")}
+          >
+            <div className="col-start-1 col-end-3 row-start-1 row-end-3">
+              <Image
+                src={yt1}
+                className="w-full h-full object-cover"
+                alt="Image 1"
+              />
+            </div>
+            <div className="col-start-3 col-end-5 row-start-1 row-end-3">
+              <Image
+                src={yt2}
+                className="w-full h-full object-cover"
+                alt="Image 2"
+              />
+            </div>
+            <div className="col-start-5 col-end-9 row-start-1 row-end-6">
+              <Image
+                src={yt3}
+                className="w-full h-full object-cover"
+                alt="Image 3"
+              />
+            </div>
+            <div className="col-start-1 col-end-5 row-start-3 row-end-6">
+              <Image
+                src={yt4}
+                className="w-full h-full object-cover"
+                alt="Image 4"
+              />
+            </div>
+            {/* Insira os outros elementos de imagem aqui */}
+            {isHovered && (
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                <div className="">
+                  <span className="text-white text-3xl">
+                    Assista nosso vídeo
+                  </span>
+                  <div className="flex justify-center">
+                    <Icon
+                      icon="play"
+                      svgProps={{ fill: "#fff", width: "80px" }}
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div>
+            <div className="text-left mb-8 lg:p-4">
+              <h2 className="text-4xl font-bold text-primary">
+                Por onde estamos?
+              </h2>
+            </div>
+          </div>
+        </div>
+
+        {selectedVideo && (
+          <div className="fixed z-50 top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50">
+            <iframe
+              src={selectedVideo}
+              title="video"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              style={{ width: "80%", height: "80%" }}
+            />
+            <button
+              onClick={() => setSelectedVideo(null)}
+              className="absolute top-4 right-4 text-white text-2xl"
+            >
+              X
+            </button>
+          </div>
+        )}
+      </section>
+
+      <section className="h-3/5 w-full bg-off">
+        <div className="grid grid-cols-2 w-4/5 mx-auto pt-20">
+          <div className="col-span-1 flex justify-center relative z-10 bg-white">
+            <Image src={jumpGirls} alt="boat" className="w-full" />
+          </div>
+          <div className="col-span-1 z-20 relative bg-white">
+            <div className="p-8">
+              <h1 className="text-4xl font-bold text-primary">Experiência</h1>
+              <div className="w-8 h-1 mt-2 ml-1 flex justify-start bg-primary"></div>
+              <p className="text-lg pt-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam voluptatum, quibusdam, quia, quod voluptates quos
+                voluptate voluptatibus quas doloribus quidem fugiat. Quisquam
+                voluptatum, quibusdam, quia, quod voluptates quos voluptate
+                voluptatibus quas doloribus quidem fugiat.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* A footer section */}
+      <div className="bg-footer">
+        <div className="relative">
+          <Image src={bgFooter} alt="bg-footer" />
+          <div className="absolute">Conheça mais sobre nós</div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,13 @@ module.exports = {
         '5vw': '5vw',
       },
       gridTemplateRows: {
-        '6': 'repeat(2, 5vw)',
+        '8': 'repeat(8, 5vw)',
+      },
+      gridTemplateColumns: {
+        '8': 'repeat(8, 1fr)',
+      },
+      gridGap: {
+        '15': '15px',
       },
       colors: {
         'primary': '#006aa1',
@@ -33,7 +39,7 @@ module.exports = {
         'tertiary': '#002162',
         'accent': '#02100E',
         'off': '#F1F4FA',
-        'footer': '#466D7E',
+        'footer': '#122442',
       },     
       fontFamily: {
         'Marcellus': ['Marcellus', 'sans-serif'],
