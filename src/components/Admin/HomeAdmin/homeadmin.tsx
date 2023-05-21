487c26daee019cc6a7c8b20e8686f18495251b72
LezierF <lezier.f_@hotmail.com>
2023-05-20 21:30:10 -0300

    feat: criado o componente teste para uso futuro

---
+++ src/components/Admin/HomeAdmin/homeadmin.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+const HomeAdmin = () => {
+    return (
+        <div>
+            <div className='flex justify-center'>
+                <p className='text-2xl'>Dados Gerais</p>
+            </div>
+            <div >
+                <div className='flex justify-around my-10'>
+                    <div className="flex flex-col xl:w-1/3 border p-6 rounded-xl">
+                        <div className="sm:-mx-6 lg:-mx-8">
+                            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8 ">
+                                <div className="overflow-hidden">
+                                    <table className="min-w-full text-left text-sm font-light">
+                                        <thead className="border-b font-medium dark:border-neutral-500">
+                                            <tr>
+                                                <th scope="col" className="px-6 py-4">Nome</th>
+                                                <th scope="col" className="px-6 py-4">Telefone</th>
+                                                <th scope="col" className="px-6 py-4">Email</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Otto</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                                            </tr>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Jacob</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Thornton</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@fat</td>
+                                            </tr>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                                            </tr>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                                            </tr>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                                            </tr>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                                            </tr>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                                            </tr>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                                            </tr>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                                            </tr>
+                                            <tr className="border-b dark:border-neutral-500">
+                                                <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                                                <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                                                <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className=''>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeAdmin
