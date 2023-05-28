7ee4c5acb5f6b833b703a583b8c08ad968ff68a7
LezierF <lezier.f_@hotmail.com>
2023-05-28 05:28:36 -0300

    fix: removendo chamada inutilizada

--- src/components/Map/MyMap.jsx
+++ src/components/Map/MyMap.jsx
@@ -2,7 +2,7 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import 'leaflet-defaulticon-compatibility';
-import markerBoat from '../../images/svgs/boat-marker'
+
 
 const MyMap = () => {
 
