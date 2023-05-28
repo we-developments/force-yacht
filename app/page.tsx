459c466e130c24b79c38d62ee4a034fff0567480
LezierF <lezier.f_@hotmail.com>
2023-05-28 05:26:11 -0300

    feat: importação e configuração do map na page

--- app/page.tsx
+++ app/page.tsx
@@ -35,13 +35,9 @@ import dynamic from "next/dynamic";
 import CardList from "@/src/components/Cards/cards";
 import { useBoatManagement } from "@/services/boatManagement";
 
-const MyMap = dynamic(
-  () => import('../src/components/Map/MyMap'), // replace '@components/map' with your component's location
-  { 
-    loading: () => <p>A map is loading</p>,
-    ssr: false // This line is important. It's what prevents server-side render
-  }
-)
+//Map
+import MyMap from '../src/components/Map/MyMap'
+
 
 interface Boat {
   Id?: string;
